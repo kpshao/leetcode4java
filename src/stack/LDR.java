82b650902769cf3d7c065ded77958e24665e9d7e
KP Shao <kp.shao@outlook.com>
2020-03-18 22:12:11 +0800

    通过迭代的方法实现二叉树中序遍历

--- src/stack/LDR.java
+++ src/stack/LDR.java
@@ -1,7 +1,6 @@
 package stack;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LDR {
     List<Integer> ans = new ArrayList<>();
@@ -9,6 +8,34 @@ public class LDR {
     public List<Integer> inorderTraversal(TreeNode root) {
         travel(root);
         return ans;
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        Set<TreeNode> visited = new HashSet<>();
+        List<Integer> ans2 = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            if (node.left == null || visited.contains(node.left)) {
+                ans2.add(node.val);
+                stack.pop();
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+            } else {
+                stack.push(node.left);
+            }
+
+
+            visited.add(node);
+
+        }
+
+        return ans2;
     }
 
     void travel(TreeNode node) {
@@ -25,5 +52,19 @@ public class LDR {
       TreeNode left;
       TreeNode right;
       TreeNode(int x) { val = x; }
-  }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        node1.left = null;
+        node1.right = node2;
+        node2.left = node3;
+        node2.right = null;
+        node3.left = null;
+        node3.right = null;
+
+        System.out.println(new LDR().inorderTraversal2(node1));
+    }
 }
